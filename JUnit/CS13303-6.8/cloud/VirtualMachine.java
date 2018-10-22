c4d5fc731a4ba4e897993f40527e08ea26b510e3
gerardopuy <31751972+gerardopuy@users.noreply.github.com>
2018-10-22 12:08:43 -0500

    Add files via upload

---
+++ JUnit/CS13303-6.8/cloud/VirtualMachine.java
@@ -0,0 +1,255 @@
+package cloud;
+
+
+import java.util.HashMap;
+
+import cloud.util.CloudUtil;
+
+public class VirtualMachine {
+	final String id;         // Identificador único
+	int cpuCount;            // Número de CPU
+	long memoryGB;           // Memoria en GB
+	boolean powerStatus;     // Estado
+	String name;             // Nombre
+	String guestOS;          // Sistema Operativo
+	
+	/* 
+	 * VirtualMachine[1]:
+	 * Variable nics de ser un arreglo de
+	 * tipo NetworkCard a un HashMap de tipo 
+	 * HashMap<Integer, NetworkCard> 
+	 */
+	HashMap<Integer, NetworkCard> nics;  // Tarjetas de Red Virtuales
+	/*
+	 * VirtualMachine[10]
+	 * Agregar variable hdds de tipo 
+	 * HashMap<Integer, HardDiskDrive>
+	 */
+	HashMap<Integer, HardDiskDrive> hdds;            // Discos duros virtuales
+
+	/*
+	 * VirtualMachine[6]
+	 * Constructor debe validar lo siguiente:
+	 * - cpuCount(int) mayor a 0
+	 * - memoryGB(long) mayor o igual a 1024 y/o múltiplo de 1024
+	 * - name(String) mayor a 5 caracteres
+	 * - guestOS(String) mayor a 5 caracteres
+	 */
+	VirtualMachine(int cpuCount, long memoryGB, String guestOS){
+		// Escribe tu código {
+		if(!(cpuCount > 0)) {
+			System.err.println("Alerta: CPU debe ser mayor a 0");
+			
+		}
+		else {
+			this.cpuCount = cpuCount;
+		}
+		if(memoryGB < 1024) {
+			System.err.println("Alerta: Memoria debe ser mayor o igual a 1024");
+			this.memoryGB = 1024;
+		}
+		else {
+			this.memoryGB = memoryGB;
+			
+		}
+		if((this.memoryGB % 1024) != 0) {
+			System.err.println("Alerta: Memoria debe ser multiplo de 1024");
+			this.memoryGB = 1024;
+		}
+		else {
+			this.memoryGB = memoryGB;
+		}
+		System.err.println("Alerta: Se recomienda nombres mayores a 5 caracteres");
+		if(guestOS.length() < 5) {
+			System.err.println("Alerta: Se recomienda nombres mayores a 5 caracteres");
+		}
+		this.guestOS = guestOS;
+		// }
+		this.id = CloudUtil.getUUID();
+	}
+	
+	VirtualMachine(int cpuCount, long memoryGB, String name, String guestOS){
+		// Escribe tu código {
+		
+		if(!(cpuCount > 0)) {
+			System.err.println("Alerta: CPU debe ser mayor a 0");
+			
+		}
+		else {
+			this.cpuCount = cpuCount;
+		}
+		if(memoryGB < 1024) {
+			System.err.println("Alerta: Memoria debe ser mayor o igual a 1024");
+			this.memoryGB = 1024;
+		}
+		else {
+			this.memoryGB = memoryGB;
+			
+		}
+		if((this.memoryGB % 1024) != 0) {
+			System.err.println("Alerta: Memoria debe ser multiplo de 1024");
+			this.memoryGB = 1024;
+		}
+		else {
+			this.memoryGB = memoryGB;
+		}
+		if(name.length() < 5) {
+			System.err.println("Alerta: Se recomienda nombres mayores a 5 caracteres");
+		}
+		this.name = name;
+		if(guestOS.length() < 5) {
+			this.guestOS = guestOS;
+			System.err.println("Alerta: Se recomienda nombres mayores a 5 caracteres");
+		}
+		this.guestOS = guestOS;
+		// }
+		this.id = CloudUtil.getUUID();
+	}
+	
+	String getID(){
+		return this.id;
+	}
+	/*
+	 * VirtualMachine[9]
+	 * Crear método isPoweredOn regresando 
+	 * un Boolean si la VM está prendida.
+	 */
+	boolean isPoweredOn() {
+		/* Validar si la VM está prendida */
+		// Escribe tu código {
+		return this.powerStatus;
+		// }
+		}
+	/*
+	 * VirtualMachine[8]
+	 * Crear método isPoweredOff regresando 
+	 * un Boolean si la VM está apagada.
+	 */
+	boolean isPoweredOff(){
+		// Escribe tu código {
+		return !this.powerStatus;
+		// }
+	}
+	
+	int numberOfCPUs() {
+		/* Obtener el número de CPUs de la VM */
+		return this.cpuCount;
+		}
+	/* 
+	 * VirtualMachine[4]
+	 * Complementar Método powerOn: validar si la
+	 *  VM está prendida e imprimir mensaje 
+	 * informando que el estado es prendido.
+	 */
+	void powerOn() {
+		/* Encender la VM */
+		// Escribe tu código {
+		this.powerStatus = true;
+		System.out.println("La maquina virtual " + this.id + " esta prendida.");
+		// }
+		}
+	
+	/* 
+	 * VirtualMachine[5]
+	 * Método powerOff: validar si la VM está prendida 
+	 * e imprimir mensaje informando que el estado es apagado.
+	 */
+	void powerOff() {
+		// Escribe tu código {
+		this.powerStatus = false;
+		System.out.println("La maquina virtual " + this.id + " esta apagada.");
+		// }
+		}
+	
+	void setName(String name) {
+		/* Asignar nombre a la VM */
+		this.name = name;
+		}
+	/* 
+	 * VirtualMachine[2]
+	 * Complementar Método addNIC(NetworkCard nic). 
+	 * el id debe ser el valor de unitNumber
+	 */
+	void addNIC(NetworkCard nic) {
+		/* Agregar NIC a la VM */
+		// Escribe tu código {
+		this.nics.put(nic.unitNumber, nic);
+		// }
+		}
+	
+	/* 
+	 * VirtualMachine [3]
+	 * Complementar Método addNICs 
+	 * (HashMap<Integer, NetworkCard> nics)
+	 */
+	void addNICs(HashMap<Integer, NetworkCard> nics){
+		/* Inicializar nics con un arrayList*/
+		// Escribe tu código {
+		this.nics = nics;
+		// }
+	}
+	
+	/*
+	 * VirtualMachine 11
+	 * Crear Métodos addHDD(HardDiskDrive hdd) y 
+	 * addHDDs(HashMap<Integer, HardDiskDrive>) 
+	 */
+	void addHDD(HardDiskDrive hdd){
+		/* Agregar HDD a la VM */
+		// Escribe tu código {
+		this.hdds.put(hdd.unitNumber, hdd);
+		// }	
+	}
+	
+	void addHDDs(HashMap<Integer, HardDiskDrive> hdds){
+		/* Agregar HDDs a la VM */
+		// Escribe tu código {
+		this.hdds = hdds;
+		// }	
+	}
+	/*
+	 * VirtualMachine[7]
+	 * Sobrecargar el método java.lang.Object.toString() para mostrar
+	 *  la representación en String de VirtualMachine 
+	 *  como lo hacía CloudUtil.printVMInfo() incluyendo:
+	 *  - Los objetos NICs
+	 *  - Los objetos HDDs
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		// Escribe tu código {
+		sb.append("Nombre  : ");
+		sb.append(this.name);
+		sb.append("\nCPU     : ");
+		sb.append(this.cpuCount);
+		sb.append("\nMemoria : ");
+		sb.append(this.memoryGB);
+		sb.append("\nOS      : ");
+		sb.append(this.guestOS);
+		sb.append("\nEstado  : ");
+		if(this.powerStatus) {
+			sb.append("Prendido");
+		}
+		else {
+			sb.append("Apagado");
+		}
+		sb.append("\nID      : ");
+		sb.append(this.id);
+		sb.append("\nNICs    : ");
+		sb.append(this.nics.size());
+		sb.append("\nHDDs    : ");
+		sb.append(this.hdds.size());
+		long capacidadHD = 0;
+		for(int i = 1; i <= this.hdds.size(); i++) {
+			capacidadHD += this.hdds.get(i).capacityGB;
+		}
+		sb.append("(");
+		sb.append(capacidadHD);
+		sb.append(" GB)\n");
+		
+		// }
+		return sb.toString();
+	}
+}
